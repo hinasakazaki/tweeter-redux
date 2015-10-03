d0239aa551b9b9f3b90d7030255fa5592b2dd2b8
Hina Sakazaki <hinasakazaki@gmail.com>
2015-10-03 08:50:18 -0700

    first commit

---
+++ tweeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tweeter
+//
+//  Created by Hina Sakazaki on 10/3/15.
+//  Copyright © 2015 Hina Sakazaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
